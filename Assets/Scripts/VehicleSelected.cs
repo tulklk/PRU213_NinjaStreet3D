afdda0f23bd7fdb98988f52d7764a2dff3f393b8
Phan Thanh Tu <tulklk32@gmail.com>
2025-09-21 16:48:41 +0700

    add racing boy spawnwe, vehicleselected, scene, fix groundspawner

---
+++ Assets/Scripts/VehicleSelected.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSelected : MonoBehaviour
+{
+    public int currentVehicleIndex;
+    public GameObject[] vehicles;
+    void Start()
+    {
+        currentVehicleIndex = PlayerPrefs.GetInt("SelectedVehicle", 0);
+        foreach (GameObject vehicle in vehicles)
+        {
+            vehicle.SetActive(false);
+        }
+        vehicles[currentVehicleIndex].SetActive(true);
+    }
+}
